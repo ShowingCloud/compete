24c20f657103ab6510f19225cdf15b1935168184
WHT <wht@WHTdeMacBook-Pro.local>
2016-03-08 17:14:19 +0800

    judge login

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -10,39 +10,63 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+var appPara = {
+    serverHost = "http://localhost:3000/"
+}
+
+var app {
+        init: function () {
+            var tempStr, judgeInfo;
+            tempStr = localStorage.getItem("judge");
+            if (typeof (tempStr) === "string") {
+                judge = JSON.parse(judgeInfo);
+                showJudge(judge);
+            } else {
+                $$("#login-container").show();
+            }
+        }
+        , showJudge: function (data) {
+            console.log(data);
+            //ToDo:JudgeInfo templete and append to judge-container
+
+
+
+            $$("#login-container").hide();
+            $$("judge-info").show();
+        }
+
+    }
+    //Initialize PouchDB
+var score = new PouchDB("score");
+//Not use remote PouchDb server
+var remoteCouch = false;
+
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
+
+    // handle login 
+    $$('＃login-btn').on('click', function () {
+        var username = $$("#username").val();
+        var password = $$("#password").val();
+        if (typeof username === "string" && typeof password === "string") {
+            $$.ajax({
+                method: "POST"
+                , url: appPara.host + "login"
+                , success: function (d) {
+                    console.log(d);
+                    var judgeInfo = {
+                        userId: d.id
+                        , email: d.info.email
+                        , nickname: d.extra.nickname
+                        , authToken: d.info.private_token
+                    };
+                    localStorage.setItem("judgeInfo", JSON.stringify(judgeInfo));
+                    console.log('userInfo saved!');
+                    app.showjudge(judgeInfo);
+                }
+            });
+        } else {
+            alert("请输入用户名和密码");
+        }
     });
 });
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
